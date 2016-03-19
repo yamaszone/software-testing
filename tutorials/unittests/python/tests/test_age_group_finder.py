b89a43fd338a2315c14b8b72c8240ea93c7c2302
Mazedur Rahman <mazedur.rahman.liton@gmail.com>
2016-03-19 19:26:36 +0000

    Fixing broken build

--- tutorials/unittests/python/tests/test_age_group_finder.py
+++ tutorials/unittests/python/tests/test_age_group_finder.py
@@ -5,5 +5,5 @@ class test_age_group_finder(unittest.TestCase):
 
 	def test_age_group_baby(self):
 		self.assertEqual('Baby', age_group_finder.find(0))
-		self.assertEqual('Baby', age_group_finder.find(-1))
-		self.assertEqual('Baby', age_group_finder.find(-100))
+		self.assertEqual('Baby', age_group_finder.find(1))
+		self.assertEqual('Baby', age_group_finder.find(2))
